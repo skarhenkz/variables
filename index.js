0d7447f43636056ceb098201603fd2a88cc9030c
arinze <henryudeze52@gmail.com>
2020-03-24 08:15:35 +0100

    variables

---
+++ index.js
@@ -0,0 +1,30 @@
+var firstName1="arinze";
+console.log(firstName1);
+var firstName2="chbuike";
+console.log(firstName2);
+var firstName3="samuel";
+console.log(firstName3);
+var firstName4="ebuka";
+console.log(firstName4);
+var firstName5="kenneth";
+console.log(firstName5);
+
+var a=10;
+var b=15;
+var c=25;
+var d=14;
+var e=20;
+var f=a+b+c+d+e;
+console.log(f);
+
+var a=10;
+var b=20;
+var c=25;
+var d=30;
+var e=15;
+if(a==b==c==d==e){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
