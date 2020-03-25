9baa14e1f85c1e77bebc5b533e0a7076a93cbf3d
arinze <henryudeze52@gmail.com>
2020-03-25 11:45:35 +0100

    operators

---
+++ game.js
@@ -0,0 +1,12 @@
+var Game_number1=2;
+var Game_number2=4;
+var Game_number3=6;
+var Game_number4=8;
+var Game_number5=10;
+var Game_number6=prompt("type any number from 2 to 10");
+if (Game_number6%2==0){
+    alert("the number you typed is correct");
+}else{
+    alert("the number you typed is wrong");
+}
+
---
+++ operators.js
@@ -0,0 +1,33 @@
+var Assign_1 = 10;
+var Assign_2 = 12;
+if (Assign_1 == Assign_2) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+var Com_1 = 20;
+var Com_2 = "20";
+if (Com_1 === Com_2) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+var A = 2;
+var B = 4;
+var C = A + B;
+console.log(C);
+var firstName = "photo";
+var secondName = "graph";
+var Name = firstName + secondName;
+console.log(Name);
+var a = 9;
+var a = a--;
+console.log(a);
+var E = 10;
+var F = 3;
+var G = E - F;
+console.log(G);
+var I = 4;
+var J = 2;
+var K = 4 / 2;
+console.log(K);
